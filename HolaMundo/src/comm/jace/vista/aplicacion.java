ba83eb0770f40778fdc078c2fda82a9a422861a1
SuRokaneki <jayrshadowfiend@gmail.com>
2018-10-24 11:53:07 -0500

    Primera versien

---
+++ HolaMundo/src/comm/jace/vista/aplicacion.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comm.jace.vista;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Necrolyte
+ */
+public class aplicacion {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "hola mondo condo");
+        System.out.println("hola mundo");
+    }
+}
